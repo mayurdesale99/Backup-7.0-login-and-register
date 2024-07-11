7ab66ef01912e11145733ccda6bfa3fc01e7c1b1
Mayur Bhausaheb Desale <94818416+mayurdesale99@users.noreply.github.com>
2024-07-11 15:48:31 +0530

    Add files via upload

---
+++ src/Components/Header.js
@@ -0,0 +1,26 @@
+// src/Components/Header.js
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/login">CRUD App</Link>
+      <div className="collapse navbar-collapse">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/login">Login</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/register">Register</Link>
+          </li>
+          {/* <li className="nav-item">
+            <Link className="nav-link" to="/users">Users</Link>
+          </li> */}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
